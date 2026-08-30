5e2a0a5891ffe1d946135c8be26c2a02e0f9aad6
Frank Machado Graziadio <fmachado.graziadio99@gmail.com>
2026-08-30 09:42:44 -0400

    Update TelegramDL.py

--- TelegramDL.py
+++ TelegramDL.py
@@ -82,7 +82,7 @@ else:
     BASE_DIR = Path(__file__).resolve().parent
     BUNDLE_DIR = BASE_DIR
 
-APP_VERSION = "2.0.3"
+APP_VERSION = "2.0.4"
 GITHUB_REPO = "infinityxgame/tgdown"
 updater = AppUpdater(APP_VERSION, GITHUB_REPO, BASE_DIR)
 
